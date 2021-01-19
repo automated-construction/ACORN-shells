922c96c1756f0038f93af69bae0a864aab85c4e2
Mishael Nuh <mishael.ebel.nuh@gmail.com>
2021-01-19 14:42:02 +0000

    Add form_finding function.

--- src/acorn_shell/generate.py
+++ src/acorn_shell/generate.py
@@ -1,5 +1,73 @@
+from ghpythonlib import components as ghcomp
 import Rhino.Geometry as rg
 import Rhino.RhinoDoc as rd
+from System import IO
+import os
+
+def form_find(plan_surface, corners, height, run):
+    '''Use Kiwi3D to form-find the shell.
+    Uses Kiwi3D v0.5.0
+
+    Parameters:
+        plan_surface (Brep): Flat surface to be form found.
+        corners (Curve): Curves of plan corners.
+        height (float): Target height of shell.
+        run (bool): Toggle to run analysis.
+
+    Returns:
+        form_found_surface (Brep): Form found brep.
+        kiwi_errors (string): Errors from Kiwi3D.
+    '''
+    tol = rd.ActiveDoc.ModelAbsoluteTolerance
+
+    # Kiwi3D parameters
+    MAT_CONC = 5
+    SURF_DEGREE = 3
+    SURF_SUBDIV = 10
+    ANAL_OUTPUT = [1, 2, 3]
+    thickness = 0.1
+
+    tmp_directory = os.path.join(IO.Path.GetTempPath(), IO.Path.GetRandomFileName())
+    if (os.path.exists(tmp_directory)):
+        shutil.rmtree(tmp_directory)
+    os.mkdir(tmp_directory)
+
+    # Define model
+    kiwi_material = ghcomp.Kiwi3d.MaterialDefaults(MAT_CONC)[0]
+    kiwi_refinement = ghcomp.Kiwi3d.SurfaceRefinement(SURF_DEGREE,
+        SURF_DEGREE, SURF_SUBDIV, SURF_SUBDIV)
+    kiwi_shell = ghcomp.Kiwi3d.ShellElement(plan_surface, kiwi_material,
+        thickness, kiwi_refinement, None, False)
+    
+    # Pinned support at corners
+    # Divide the curves into points since the support curve seems to not work
+    # as expected
+    kiwi_supports = []
+    support_points = [c.DivideByCount(100, True) for c in corners]
+    for i in range(len(support_points)):
+        kiwi_supports.extend([ghcomp.Kiwi3d.SupportPoint(corners[i].PointAt(t),
+            True, True, True, False, False) for t in support_points[i]])
+    # Uniform load pushing upwards
+    kiwi_loads = ghcomp.Kiwi3d.SurfaceLoad(plan_surface, '1', rg.Vector3d.ZAxis,
+        100, None, None, 1)
+
+    # Run Kiwi analysis
+    kiwi_a_option = ghcomp.Kiwi3d.LinearAnalysis('FormFinding', ANAL_OUTPUT)
+    kiwi_model = ghcomp.Kiwi3d.AnalysisModel(kiwi_a_option, kiwi_shell,
+        kiwi_supports, kiwi_loads)
+    kiwi_result = ghcomp.Kiwi3d.IGASolver(kiwi_model, tmp_directory, run)
+    kiwi_errors = kiwi_result[1]
+    kiwi_result = kiwi_result[0]
+    
+    # Scale deformed shell to target height
+    form_found_surface = ghcomp.Kiwi3d.DeformedModel(kiwi_result)[1]
+    if (form_found_surface != None):
+        bounds = form_found_surface.GetBoundingBox(rg.Plane.WorldXY)
+        scale = height/(bounds.Max.Z - bounds.Min.Z)
+        form_found_surface.Transform(
+            rg.Transform.Scale(rg.Plane(bounds.Min, rg.Vector3d.ZAxis), 1, 1, scale))
+
+    return [form_found_surface, kiwi_errors]
 
 def make_2d_plan(outline, corner_radius):
     '''Creates a 2D shell plan on the XY plane.
