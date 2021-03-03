9ecddfd6c0964dca7b620525aca82e6fcfbc642b
Eduardo Costa <castroecosta@gmail.com>
2021-03-03 00:41:43 +0000

    Added tree support in Pinbed components
    FitModules outputs tree
    AreaEfficiency inputs tree

--- ACORN-shells/GetAreaEfficiency.cs
+++ ACORN-shells/GetAreaEfficiency.cs
@@ -2,16 +2,19 @@
 using System.Collections.Generic;
 
 using Grasshopper.Kernel;
+using Grasshopper.Kernel.Data;
+using Grasshopper.Kernel.Types;
 using Rhino.Geometry;
 using System.Linq;
 using Rhino.Geometry.Collections;
 using Rhino.Collections;
 using GH.MiscToolbox.Components.Utilities;
+using Grasshopper;
 
 namespace ACORN_shells
 {
     /// <summary>
-    /// Fits a bounding box to a shell segment, minimizing volume.
+    /// Calculates efficiency
     /// </summary>
     public class GetAreaEfficiency : GH_Component
     {
@@ -24,41 +27,84 @@ namespace ACORN_shells
 
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
-            pManager.AddBrepParameter("Segment", "S", "Shell segment.", GH_ParamAccess.item);
-            pManager.AddRectangleParameter("Modules", "M", "Rectangles corresponding to modules", GH_ParamAccess.list); //change to tree?
-
+            pManager.AddBrepParameter("Segment", "S", "Shell segment", GH_ParamAccess.tree);
+            pManager.AddRectangleParameter("Modules", "M", "Rectangles corresponding to modules", GH_ParamAccess.tree);
         }
 
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
         {
-            pManager.AddNumberParameter("Area efficiency", "E", "Area efficiency of segment / modules.", GH_ParamAccess.item);
+            pManager.AddNumberParameter("Area efficiency", "E", "Area efficiency of segment / modules.", GH_ParamAccess.tree);
+            //pManager.AddRectangleParameter("Modules", "M", "Rectangles corresponding to modules (flattened tree)", GH_ParamAccess.tree); //debug
         }
 
         protected override void SolveInstance(IGH_DataAccess DA)
         {
-            Brep segment = null;
-            List<Rectangle3d> modules = new List<Rectangle3d>();
 
-            if (!DA.GetData(0, ref segment)) return;
-            if (!DA.GetDataList(1, modules)) return;
+            GH_Structure<GH_Brep> ghSegmentTree = new GH_Structure<GH_Brep>();
+            GH_Structure<GH_Rectangle> ghModuleTree = new GH_Structure<GH_Rectangle>();
 
-            double modulesTotalArea = 0;
+            if (!DA.GetDataTree<GH_Brep>(0, out ghSegmentTree)) return;
+            if (!DA.GetDataTree<GH_Rectangle>(1, out ghModuleTree)) return;
+
             
-            //get area for module set 
-            foreach (Rectangle3d module in modules) modulesTotalArea += module.Area;
+            // collect all rectangles per segment into a DataTree
+            // since the input is GH_Structure, needs casting to RhinoCommon
+            DataTree<Rectangle3d> flatModuleTree = new DataTree<Rectangle3d>();
 
-            // get segment edge curve
-            List<Curve> edges = new List<Curve>();
-            foreach (BrepEdge edge in segment.Edges) edges.Add(edge.ToNurbsCurve());
-            Curve joinedSegmentEdges = Curve.JoinCurves(edges)[0];
+            foreach (GH_Path path in ghModuleTree.Paths)
+            {
+                // look at first index corresponding to segments
+                int currSegment = path.Indices[0];
+                // creates a branch to store the modules in current segment in flatModuleTree
+                GH_Path flatPath = new GH_Path(currSegment);
 
-            // get segment area, projected onto module plane
-            Curve projectedSegment = Curve.ProjectToPlane (joinedSegmentEdges, modules[0].Plane);
-            double segmentProjectedArea = AreaMassProperties.Compute(projectedSegment).Area;
+                // casting GrassHopper to RhinoCommon objects
+                Rectangle3d rcModule = new Rectangle3d();
+                GH_Rectangle ghModule = ghModuleTree.get_Branch(path)[0] as GH_Rectangle;
+                GH_Convert.ToRectangle3d(ghModule, ref rcModule, GH_Conversion.Both);
 
-            double efficiency = segmentProjectedArea / modulesTotalArea;
+                flatModuleTree.Add(rcModule, flatPath);
+            }
 
-            DA.SetData(0, efficiency);
+
+            // convert segmentTree: GH_Structure (Grasshopper) to DataTree (RhinoCommon)
+            DataTree<Brep> rcSegmentTree = new DataTree<Brep>();
+            foreach (GH_Path path in ghSegmentTree.Paths)
+            {
+                GH_Brep ghSegment = ghSegmentTree.get_Branch(path)[0] as GH_Brep;
+                Brep rcSegment = new Brep();
+                GH_Convert.ToBrep(ghSegment, ref rcSegment, GH_Conversion.Both);
+                rcSegmentTree.Add(rcSegment, path);
+            }
+
+            DataTree<double> efficiencyTree = new DataTree<double>();
+
+            // assuming tree structure in rcSegmentTree and flatModuleTree is the same: {one}(many)
+            foreach (GH_Path path in flatModuleTree.Paths)             
+            {
+                //get area for module set 
+                double modulesTotalArea = 0;                              
+                foreach (Rectangle3d module in flatModuleTree.Branch(path)) 
+                    modulesTotalArea += module.Area;
+
+                // get segment edge curve
+                List<Curve> edges = new List<Curve>();
+                Brep currSegment = rcSegmentTree.Branch(path)[0];
+                foreach (BrepEdge edge in currSegment.Edges) edges.Add(edge.ToNurbsCurve());
+                Curve joinedSegmentEdges = Curve.JoinCurves(edges)[0];
+
+                // get segment area, projected onto module plane
+                List<Rectangle3d> modules = flatModuleTree.Branch(path);
+                Curve projectedSegment = Curve.ProjectToPlane (joinedSegmentEdges, modules[0].Plane);
+                double segmentProjectedArea = AreaMassProperties.Compute(projectedSegment).Area;
+
+                double currEfficiency = segmentProjectedArea / modulesTotalArea;
+                efficiencyTree.Add(currEfficiency, path);
+            }
+
+
+            DA.SetDataTree(0, efficiencyTree);
+            //DA.SetDataTree(1, flatModuleTree); //debug
         }
 
         protected override System.Drawing.Bitmap Icon
---
+++ ACORN-shells/SimulatePinbed.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+using System.Linq;
+using Rhino.Geometry.Collections;
+using Rhino.Collections;
+using GH.MiscToolbox.Components.Utilities;
+
+namespace ACORN_shells
+{
+    /// <summary>
+    /// Fits a bounding box to a shell segment, minimizing volume.
+    /// </summary>
+    public class SimulatePinbed : GH_Component
+    {
+        public SimulatePinbed()
+          : base("Simulate Pinbed", "A:SimPinbed",
+              "Simulates the pinbed mould.",
+              "ACORN", "Pinbed")
+        {
+        }
+
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddBoxParameter("Box", "B", "Shell segment bounding box.", GH_ParamAccess.item);
+            pManager.AddNumberParameter("Width", "W", "Module width [m]", GH_ParamAccess.item);
+            pManager.AddNumberParameter("Depth", "D", "Module depth [m]", GH_ParamAccess.item);
+            pManager.AddNumberParameter("Tolerance Level", "T", "Tolerance = 1e-(tolLevel)", GH_ParamAccess.item);
+
+        }
+
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddRectangleParameter("Module rectangles", "M", "Set of pinbed module rectangles.", GH_ParamAccess.list);
+        }
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            Box box = Box.Unset;
+            double moduleWidth = 1;
+            double moduleDepth = 1;
+            double tolLevel = 3;
+
+            if (!DA.GetData(0, ref box)) return;
+            if (!DA.GetData(1, ref moduleWidth)) return;
+            if (!DA.GetData(2, ref moduleDepth)) return;
+            if (!DA.GetData(3, ref tolLevel)) return;
+
+            // calculate number of modules in both width and depth
+            //double tol = DocumentTolerance(); // in case segment size is exact multiple of module size (e.g. keystone)
+            double tol = Math.Exp(-tolLevel); // in case segment size is exact multiple of module size (e.g. keystone)
+
+            double boxWidth = box.X.Max - box.X.Min - tol;
+            double boxDepth = box.Y.Max - box.Y.Min - tol;
+            int modulesInWidth = (int)Math.Ceiling(boxWidth / moduleWidth);
+            int modulesInDepth = (int)Math.Ceiling(boxDepth / moduleDepth);
+
+            // find bottom plane
+            Plane bottomPlane = new Plane(box.Plane);
+            bottomPlane.Origin = box.PointAt(0.5, 0.5, 0);
+            // move bottom plane origin in order to match rects union center with initial bottomplane origin
+            bottomPlane.Origin = bottomPlane.PointAt(- modulesInWidth * moduleWidth / 2, - modulesInDepth * moduleDepth / 2);
+
+            // draw rectangles
+            List<Rectangle3d> rects = new List<Rectangle3d>();
+            for (int w = 0; w < modulesInWidth; w++)
+            {
+                for (int d = 0; d < modulesInDepth; d++) 
+                {
+                    Interval domainWidth = new Interval(w * moduleWidth, (w + 1) * moduleWidth);
+                    Interval domainDepth = new Interval(d * moduleDepth, (d + 1) * moduleDepth);
+                    Rectangle3d module = new Rectangle3d(bottomPlane, domainWidth, domainDepth);
+                    rects.Add(module);
+                }                    
+            }
+
+            DA.SetDataList(0, rects);
+        }
+
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                return ACORN_shells.Properties.Resources.ACORN_24;
+            }
+        }
+
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("3137bd7c-0f11-4688-b9dc-c57f003eb552"); }
+        }
+    }
+}
