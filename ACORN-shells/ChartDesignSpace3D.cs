40fc7ba471b5e746d19b5e2a470c486094e2badd
Eduardo Costa <castroecosta@gmail.com>
2021-06-18 13:24:20 +0100

    Updated DSV
    Changed component name: SectionDesignSpace -> ChartDesignSpace3D
    Added functionality to ChartDesignSpace3D

---
+++ ACORN-shells/ChartDesignSpace3D.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Data;
+using Rhino.Geometry;
+using System.Linq;
+using Rhino.Geometry.Collections;
+using Rhino.Collections;
+using GH.MiscToolbox.Components.Utilities;
+using System.Drawing;
+using Grasshopper.Kernel.Types;
+using DSVcommon;
+
+namespace ACORN_shells
+{
+    /// <summary>
+    /// Sections a design space from DSE
+    /// 
+    /// NOT BEING COMPILED
+    /// </summary>
+    public class ChartDesignSpace3D : GH_Component
+    {
+        public ChartDesignSpace3D()
+          : base("Chart Design Space 3D", "A:ChartDS-3D",
+              "Charts a Design Space in 3D according to fixed dimensions",
+              "ACORN", "DSV")
+        {
+        }
+
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddIntegerParameter("Varying dimensions", "VD", "Varying dimensions that define section", GH_ParamAccess.list);
+            pManager.AddGenericParameter("Design space", "DS", "Design Vectors in the design space", GH_ParamAccess.list);
+            pManager.AddNumberParameter("Reference vector components", "RVC", "Reference vector components", GH_ParamAccess.list);
+            pManager.AddIntegerParameter("Result to map", "R", "Result to map", GH_ParamAccess.item);
+            pManager.AddBoxParameter("Chart Box", "B", "Box containing the chart", GH_ParamAccess.item);
+        }
+
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddGenericParameter("Design Vectors in section", "S", "Design Vectors in section", GH_ParamAccess.list);
+            pManager.AddGenericParameter("Closest Design Vector", "C", "Design Vector closest to reference vector", GH_ParamAccess.item);
+            pManager.AddTextParameter("Closest Vector Info", "I", "Information on closest vector", GH_ParamAccess.item);
+            pManager.AddPointParameter("Sectioned space points", "P", "Points corresponding to sectioned Design Vectors", GH_ParamAccess.list);
+            pManager.AddBoxParameter("Space Box", "B", "Box containing Design Space points", GH_ParamAccess.item);
+            pManager.AddPointParameter("Mapped points", "M", "Points corresponding to mapped sectioned Design Vectors", GH_ParamAccess.list);
+            pManager.AddGeometryParameter("Chart axes", "A", "Chart axes", GH_ParamAccess.list);
+        }
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+
+            List<int> varyingDimensions = new List<int>();
+            List<DesignVector> designSpace = new List<DesignVector>();
+            List<double> referenceVectorComponents = new List<double>();
+            int resultToMap = 0;
+            Box ghChartBox = Box.Unset;
+
+            if (!DA.GetDataList(0, varyingDimensions)) return;
+            if (!DA.GetDataList(1, designSpace)) return;
+            if (!DA.GetDataList(2, referenceVectorComponents)) return;
+            if (!DA.GetData(3, ref resultToMap)) return; 
+            if (!DA.GetData(4, ref ghChartBox)) return;
+
+            // convert Box to BoundingBox
+            BoundingBox chartBox = ghChartBox.BoundingBox;
+
+            List<DesignVector> sectionedSpace = DesignVector.SectionDesignSpace
+                (varyingDimensions, designSpace, referenceVectorComponents, out DesignVector closestVector);
+
+            string closestVectorInfo = closestVector.DescribeVector();
+
+            List<Point3d> designSpacePoints = DesignVector.Make3DChart
+                (designSpace, sectionedSpace, varyingDimensions, resultToMap, out BoundingBox spaceBox);
+
+            List<Point3d> mappedPoints = DesignVector.MapDesignVectorsToChart(designSpacePoints, spaceBox, chartBox);
+
+            List<GeometryBase> axesElements = DesignVector.MakeChartAxes(chartBox, designSpace, varyingDimensions);
+
+            DA.SetDataList(0, sectionedSpace);
+            DA.SetData(1, closestVector);
+            DA.SetData(2, closestVectorInfo);
+            DA.SetDataList(3, designSpacePoints);
+            DA.SetData(4, spaceBox);
+            DA.SetDataList(5, mappedPoints); 
+            DA.SetDataList(6, axesElements);
+        }
+
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                return ACORN_shells.Properties.Resources.ACORN_24;
+            }
+        }
+
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("dd1fe2e9-65e0-4e37-8ae0-dbf0b0f1789c"); }
+        }
+    }
+}
--- ACORN-shells/DSVcommon.cs
+++ ACORN-shells/DSVcommon.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Rhino.Geometry;
+
 namespace DSVcommon
 {
     public class DesignVector
@@ -16,11 +18,13 @@ namespace DSVcommon
 
         public DesignVector() { } // basic constructor
 
-        public DesignVector(List<double> designMap) // constructor for Reference Vector: no ID or Objective Values
+        public DesignVector
+            (List<double> designMap) // constructor for Reference Vector: no ID or Objective Values
         {
             this.DesignMap = designMap;
         }
-        public DesignVector(int id, List<double> designMap, List<double> objValues) // constructor for DV with Design Map and Objective Values
+        public DesignVector
+            (int id, List<double> designMap, List<double> objValues) // constructor for DV with Design Map and Objective Values
         {
             this.ID = id;
             this.DesignMap = designMap;
@@ -33,7 +37,8 @@ namespace DSVcommon
         /// <param name="v1"></param>
         /// <param name="v2"></param>
         /// <returns></returns>
-        public static double GetVectorDistance (DesignVector v1, DesignVector v2)
+        public static double GetVectorDistance 
+            (DesignVector v1, DesignVector v2)
         {
             double squaredDistance = 0;
             // iterate each dimension in the Design Map for calculating vector component squared distances
@@ -49,7 +54,8 @@ namespace DSVcommon
         /// <param name="refVector"></param>
         /// <param name="closestDistance"></param>
         /// <returns></returns>
-        public static DesignVector FindClosestVector (List<DesignVector> designSpace, DesignVector refVector, out double closestDistance)
+        public static DesignVector FindClosestVector 
+            (List<DesignVector> designSpace, DesignVector refVector, out double closestDistance)
         {
             DesignVector closestVector = designSpace[0];
             closestDistance = GetVectorDistance(closestVector, refVector);
@@ -65,6 +71,181 @@ namespace DSVcommon
                 }
             }
             return closestVector;
+        }
+        /// <summary>
+        /// Returns a string describing the DesignVector
+        /// </summary>
+        /// <returns></returns>
+        public string DescribeVector()
+        {
+            string description = "";
+            description += "ID: " + ID.ToString() + "\n";
+
+            for (int i = 0; i < DesignMap.Count; i++)
+                description += DesignMapLabels[i] + ": " + DesignMap[i].ToString() + "\n";
+            for (int j = 0; j < ObjValues.Count; j++)
+                description += ObjValuesLabels[j] + ": " + ObjValues[j].ToString() + "\n";
+            
+            return description;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="varyingDimensions"></param>
+        /// <param name="designSpace"></param>
+        /// <param name="referenceVectorComponents"></param>
+        /// <param name="closestVector">out parameter - </param>
+        /// <returns></returns>
+        public static List<DesignVector> SectionDesignSpace
+            (List<int> varyingDimensions, List<DesignVector> designSpace, List<double> referenceVectorComponents, out DesignVector closestVector)
+        {
+            // find vector in DS closest to refVector
+            DesignVector referenceVector = new DesignVector(referenceVectorComponents);
+            closestVector = DesignVector.FindClosestVector(designSpace, referenceVector, out _);
+
+            // determine fixed dimensions from varied - move to DSVcommon?
+            List<double> fixedDimensions = new List<double>();
+            double numberOfDimensions = designSpace[0].DesignMap.Count;
+            for (int dim = 0; dim < numberOfDimensions; dim++)
+                if (!varyingDimensions.Contains(dim)) fixedDimensions.Add(dim);
+
+            // find vectors in section, i.e., that have ALL the same values in the Fixed Dimensions as the reference vector
+            List<DesignVector> sectionedSpace = new List<DesignVector>();
+            foreach (DesignVector dv in designSpace)
+            {
+                List<bool> inSection = new List<bool>();
+                foreach (int fixedDimension in fixedDimensions)
+                {
+                    if (dv.DesignMap[fixedDimension] == referenceVector.DesignMap[fixedDimension]) inSection.Add(true);
+                    else inSection.Add(false);
+                }
+                // if inSection only contains Trues, then add vector to sectioned space
+                if (!inSection.Contains(false)) sectionedSpace.Add(dv);
+            }
+
+            return sectionedSpace;
+        }
+
+        public static List<Point3d> Make3DChart // change name eventually...
+            (List<DesignVector> designSpace, List<DesignVector> sectionedSpace, List<int> varyingDimensions, int resultToMap, out BoundingBox sectionedSpaceBox)
+        {
+            // corrected Result int, from obsolete GH implementation - to be improved
+            int corrResultToMap = resultToMap - sectionedSpace[0].DesignMap.Count;
+
+            // make 3D points from DVs in sectioned Space
+            List<Point3d> sectionedSpacePoints = new List<Point3d>(); // main output?
+            foreach (DesignVector dv in sectionedSpace)
+            {
+                List<double> pointCoords = new List<double>();
+                foreach (int dim in varyingDimensions)
+                    pointCoords.Add(dv.DesignMap[dim]);        
+                pointCoords.Add(dv.ObjValues[corrResultToMap]);
+                sectionedSpacePoints.Add (new Point3d(pointCoords[0], pointCoords[1], pointCoords[2]));
+            }
+
+            // make bounding box for sectioned space
+            sectionedSpaceBox = new BoundingBox(sectionedSpacePoints);
+
+            // get domain of resultToMap in the whole design space, not only sectioned space
+            // for extending bounding box so that charts are consistent for the same result
+            List<double> allValuesForResult = new List<double>();
+            foreach (DesignVector dv in designSpace)             
+                allValuesForResult.Add(dv.ObjValues[corrResultToMap]);
+            sectionedSpaceBox = new BoundingBox
+                (sectionedSpaceBox.Min.X, sectionedSpaceBox.Min.Y, allValuesForResult.Min(),
+                sectionedSpaceBox.Max.X, sectionedSpaceBox.Max.Y, allValuesForResult.Max());
+
+            return sectionedSpacePoints;
+
+        }
+
+        public static List<GeometryBase> MakeChartAxes(BoundingBox chartBox, List<DesignVector> designSpace, List<int> varyingDimensions)
+        {
+            List<GeometryBase> axesElements = new List<GeometryBase>();
+            // make axes lines
+            Curve xAxis = new Line(chartBox.Min, new Vector3d(chartBox.Diagonal.X * Vector3d.XAxis)).ToNurbsCurve();
+            Curve yAxis = new Line(chartBox.Min, new Vector3d(chartBox.Diagonal.Y * Vector3d.YAxis)).ToNurbsCurve();
+            Curve zAxis = new Line(chartBox.Min, new Vector3d(chartBox.Diagonal.Z * Vector3d.ZAxis)).ToNurbsCurve();
+
+            axesElements.AddRange(new List<GeometryBase>() { xAxis, yAxis, zAxis });
+
+            // set axes domains from varying dimensions and result -- repeats a lot (see Make3D chart ln 152)
+            List<double> allValuesForX = new List<double>();
+            foreach (DesignVector dv in designSpace)
+                allValuesForX.Add(dv.ObjValues[varyingDimensions[0]]);
+            Interval xAxisDomain = new Interval(allValuesForX.Min(), allValuesForX.Max());
+
+            axesElements.AddRange(MakeSingleAxis(xAxis, xAxisDomain));
+            //axesElements.AddRange(MakeSingleAxis(yAxis));
+            //axesElements.AddRange(MakeSingleAxis(zAxis));
+
+            return axesElements;
+        }
+
+        public static List<GeometryBase> MakeSingleAxis(Curve axisLine, Interval axisDomain)
+        {
+            int nrDivs = 7; // make dependent on nr vectors per dimension...
+            double axisDotLength = .05;
+            
+            List<GeometryBase> axisElements = new List<GeometryBase>();
+
+            // divide axes lines and assign values - based on the whole Design Space
+            Plane[] divPlanes = axisLine.GetPerpendicularFrames(axisLine.DivideByCount(nrDivs - 1, true));
+
+            List<Curve> axisDots = new List<Curve>();
+            List<TextDot> axisTextDots = new List<TextDot>();
+            double step = axisDomain.Min; // for iterating axis values
+
+            foreach (Plane divPlane in divPlanes)
+            {
+                // adds a spoke to the axis line
+                axisDots.Add(new Line(divPlane.Origin, new Vector3d(divPlane.XAxis * -axisDotLength)).ToNurbsCurve());
+                // adds a TextDot with the current axis value
+                axisTextDots.Add(new TextDot (step.ToString(), divPlane.PointAt(-axisDotLength, 0))); 
+                // update axis value
+                step += axisDomain.Length / (nrDivs - 1);
+            }
+            axisElements.AddRange(axisDots);
+            axisElements.AddRange(axisTextDots);
+
+            return axisElements;
+        }
+
+        public static List<Point3d> MapDesignVectorsToChart (List<Point3d> points, BoundingBox sourceBox, BoundingBox targetBox)
+        {
+            List<Point3d> transformedPoints = new List<Point3d>();
+
+            // create Transform for recreating BoxMap component
+            /*
+            Transform boxScaling = Transform.ChangeBasis
+                (Vector3d.XAxis * sourceBox.Diagonal.X, Vector3d.YAxis * sourceBox.Diagonal.Y, Vector3d.ZAxis * sourceBox.Diagonal.Z,
+                Vector3d.XAxis * targetBox.Diagonal.X, Vector3d.YAxis * targetBox.Diagonal.Y, Vector3d.ZAxis * targetBox.Diagonal.Z);
+            */
+
+            // define origin plane of sourceBox
+            Plane sourceOrigin = Plane.WorldXY;
+            sourceOrigin.Origin = sourceBox.Min;
+
+            Transform boxScaling = Transform.Scale(sourceOrigin,
+                targetBox.Diagonal.X / sourceBox.Diagonal.X, 
+                targetBox.Diagonal.Y / sourceBox.Diagonal.Y, 
+                targetBox.Diagonal.Z / sourceBox.Diagonal.Z);
+            Transform boxMoving = Transform.Translation(new Vector3d(targetBox.Min) - new Vector3d (sourceBox.Min));
+            Transform boxMapping = Transform.Multiply(boxMoving, boxScaling);
+
+
+            foreach (Point3d point in points)
+            {
+                Point3d transformedPoint = new Point3d(point);
+                transformedPoint.Transform(boxMapping);
+                transformedPoints.Add(transformedPoint);
+            }
+
+
+
+
+            return transformedPoints;
         }
 
     }
