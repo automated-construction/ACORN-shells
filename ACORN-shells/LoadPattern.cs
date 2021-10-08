42cb6f3ced67ff9a54ee8a9325a5787080482af5
Eduardo Costa <castroecosta@gmail.com>
2021-10-08 22:49:59 +0100

    More flexibility to Load Patterns
    Added support for multiple load patterns, translated into respective Karamba load cases
    Separated ShellLoads from LoadPattern

---
+++ ACORN-shells/LoadPattern.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Data;
+using Grasshopper.Kernel.Types;
+using Rhino.Geometry;
+using Rhino.Geometry.Collections;
+using Karamba.GHopper.Geometry;
+using Karamba.Geometry;
+using Karamba.GHopper.Loads;
+using Karamba.Utilities;
+using Karamba.Loads;
+using KarambaCommon;
+
+namespace ACORN_shells
+{
+    public class LoadPattern : GH_Component
+    {
+        public LoadPattern()
+          : base("Load Pattern", "A:LoadPattern",
+              "Creates load pattern",
+              "ACORN", "Shells")
+        {
+        }
+
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddMeshParameter("Shell mesh", "SM", "Meshed shell.", GH_ParamAccess.list);
+            pManager.AddNumberParameter("Arc", "A", "Arc", GH_ParamAccess.item);
+            pManager.AddNumberParameter("Offset", "O", "Offset", GH_ParamAccess.item);
+            pManager.AddIntegerParameter("Predefined", "D", "Predefined pattern (optional)", GH_ParamAccess.item);
+
+            pManager[3].Optional = true;
+        }
+
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddGenericParameter("Load Pattern Tuple", "T", "Indexes for faces in the pattern", GH_ParamAccess.item);
+            pManager.AddPointParameter("CheckPoints", "P", "Points for testing load patterns", GH_ParamAccess.list);
+        }
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            List<Mesh> shellMeshes = new List<Mesh>();
+            double arc = 0;
+            double offset = 0;
+            int predef = 0;
+
+            if (!DA.GetDataList(0, shellMeshes)) return;
+            if (!DA.GetData(1, ref arc)) return;
+            if (!DA.GetData(2, ref offset)) return; 
+            DA.GetData(3, ref predef);
+
+            //GH_Structure<GH_Point> checkPointTree = new GH_Structure<GH_Point>();
+            //GH_Structure<GH_Integer> indexTree = new GH_Structure<GH_Integer>();
+            DataTree<Tuple<double, double>> loadPatternTuples= new DataTree<Tuple<double, double>> ();
+            List<Point3d> checkPoints = new List<Point3d>();
+
+            // if predefined pattern, overrides input arc and offset
+            switch (predef)
+            {
+                case 1:
+                    arc = Math.PI * 2.00;
+                    offset = Math.PI * 2.00;
+                    break;
+                case 2:
+                    arc = Math.PI * 1.00;
+                    offset = Math.PI * 0.50;
+                    break;
+                case 3:
+                    arc = Math.PI * 1.00;
+                    offset = Math.PI * 0.25;
+                    break;
+                case 4:
+                    arc = Math.PI * 0.50;
+                    offset = Math.PI * 0.50;
+                    break;
+                case 5:
+                    arc = Math.PI * 0.50;
+                    offset = Math.PI * 0.25;
+                    break;
+                case 6:
+                    arc = Math.PI * 0.25;
+                    offset = Math.PI * 0.00;
+                    break;
+                case 7:
+                    arc = Math.PI * 2.00;
+                    offset = Math.PI * 0.00;
+                    break;
+                
+            }
+
+            // safety conditions
+            if (arc <= 0) return;
+
+            Tuple<double, double> loadPatternTuple = new Tuple<double,double> (arc, offset);
+
+            // illustrate current pattern
+
+            // determine shell center for relative polar coordinates of face centers FOR LIVELOAD
+            BoundingBox shellBox = BoundingBox.Unset;
+            foreach (Mesh shellMesh in shellMeshes)
+                shellBox = BoundingBox.Union (shellBox, shellMesh.GetBoundingBox(false));
+            Point3d shellCenter = shellBox.Center;
+
+
+            for (int i = 0; i<shellMeshes.Count;i++)
+            {
+                Mesh shellMesh = shellMeshes[i];
+
+                //GH_Path path = new GH_Path(i);
+
+
+                MeshFaceList k3dMeshFaces = shellMesh.Faces;
+
+                for (int faceIndex = 0; faceIndex < shellMesh.Faces.Count; faceIndex ++)
+                {
+                    // get face center polar coordinate
+                    Point3d faceCenter = k3dMeshFaces.GetFaceCenter(faceIndex);
+
+                    // ensures angle always positive, [0, 2Pi] 
+                    Vector3d faceCenterOrientation = new Vector3d(faceCenter) - new Vector3d(shellCenter);
+                    double faceAngle = Math.Atan2(faceCenterOrientation.Y, faceCenterOrientation.X);
+                    if (faceAngle < 0) faceAngle += Math.PI * 2; 
+
+                    // check if polar coordinate of face center is within load patterndomain, 
+                    
+
+
+                    if (AngleInPattern (faceAngle, loadPatternTuple))
+                    {
+                        //indexTree.Append(new GH_Integer(faceIndex), path);
+                        checkPoints.Add(faceCenter);
+
+                    }
+
+                }
+            }
+            
+            
+            DA.SetData(0, loadPatternTuple);
+            DA.SetDataList(1, checkPoints);
+        }
+
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                return ACORN_shells.Properties.Resources.ACORN_24;
+            }
+        }
+
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("fe8a728b-4aba-430f-a614-e9b2ef20cf4f"); }
+        }
+
+        public bool AngleInPattern(double angle, Tuple<double, double> loadPatternTuple)
+        {
+            double arc = loadPatternTuple.Item1;
+            double offset = loadPatternTuple.Item2;
+            double start = offset;
+            double end;
+            while (start < Math.PI * 2) // limits verification to one "lap"
+            {
+                end = start + arc;
+                // angle within interval
+                if ( (angle >= start && angle <= end) || (angle <= offset - arc)) 
+                    return true;
+                start += (2 * arc);
+            }
+            return false;
+        }
+    }
+}
--- ACORN-shells/ShellLoads.cs
+++ ACORN-shells/ShellLoads.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 
+using Grasshopper;
 using Grasshopper.Kernel;
+using Grasshopper.Kernel.Data;
+using Grasshopper.Kernel.Types;
 using Rhino.Geometry;
 using Rhino.Geometry.Collections;
 using Karamba.GHopper.Geometry;
@@ -42,7 +45,7 @@ namespace ACORN_shells
             pManager.AddMeshParameter("Shell mesh", "SM", "Meshed shell.", GH_ParamAccess.list);
             pManager.AddNumberParameter("Dead Load value", "DLV", "Dead Load value [kN/m2].", GH_ParamAccess.item);
             pManager.AddNumberParameter("Live Load value", "LLV", "Live Load value [kN/m2].", GH_ParamAccess.item);
-            pManager.AddIntegerParameter("Live Load pattern", "LLP", "Load pattern.", GH_ParamAccess.item);
+            pManager.AddGenericParameter("Live Load pattern", "LLP", "Load pattern.", GH_ParamAccess.list);
             pManager.AddNumberParameter("Area adjustment factors", "AAF", "Area adjustment factors for testing gap", GH_ParamAccess.list); //TEST for area adjustements
 
 
@@ -52,7 +55,7 @@ namespace ACORN_shells
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
         {
             pManager.AddGenericParameter("Loads", "L", "Loads for Karamba3D model.", GH_ParamAccess.list);
-            pManager.AddPointParameter("CheckPoints", "CP", "Points for testing load patterns", GH_ParamAccess.list);
+            pManager.AddPointParameter("CheckPoints", "CP", "Points for testing load patterns", GH_ParamAccess.tree);
         }
 
         protected override void SolveInstance(IGH_DataAccess DA)
@@ -60,102 +63,125 @@ namespace ACORN_shells
             List<Mesh> shellMeshes = new List<Mesh>();
             double deadLoadValue = 0;
             double liveLoadValue = 0;
-            int loadPattern = 0;
+            List<Tuple<double, double>> loadPatterns = new List<Tuple<double, double>>();
+            //List<int> loadPatterns = new List<int>(); // for multiple load patterns as load cases
             List<double> areaFactors= new List<double>();// TEST for area adjustements
 
             if (!DA.GetDataList(0, shellMeshes)) return;
             if (!DA.GetData(1, ref deadLoadValue)) return;
             if (!DA.GetData(2, ref liveLoadValue)) return;
-            if (!DA.GetData(3, ref loadPattern)) return;
+            if (!DA.GetDataList(3, loadPatterns)) return;
             DA.GetDataList(4, areaFactors); //TEST for area adjustements
 
             // Create Karamba loads
             List<Load> k3dLoads = new List<Karamba.Loads.Load>();
+            GH_Structure <GH_Point> checkPointTree = new GH_Structure<GH_Point>();
 
-            // Gravitational load
-
-            // if area factors, average them out for gravity - 
-            // should be done individually, but that makes the component complicated, needing volume and density
-            // to be revised for variable thickness
-            double areaFactorGravity = 0;
-            if (areaFactors.Count > 0)
+            int loadCase = 0;
+            foreach (Tuple<double, double> loadPattern in loadPatterns)
             {
-                foreach (double areaFactor in areaFactors)
-                    areaFactorGravity += areaFactor;
-                areaFactorGravity /= areaFactors.Count; 
-            }
-            else
-                areaFactorGravity = 1;
+                GH_Path path = new GH_Path(loadCase); // for checkPoints, for testing
 
-            Load gravity = new GravityLoad(new Vector3(0, 0, -1 * areaFactorGravity * DL_FACTOR));
-            k3dLoads.Add(gravity);
 
-            var k3dFL = new KarambaCommon.Factories.FactoryLoad();
-            UnitsConversionFactory ucf = UnitsConversionFactories.Conv();
-            UnitConversion m = ucf.m();
+                // Gravitational load
 
-            // determine shell center for relative polar coordinates of face centers FOR LIVELOAD
-            BoundingBox shellBox = BoundingBox.Unset;
-            foreach (Mesh shellMesh in shellMeshes)
-                shellBox = BoundingBox.Union (shellBox, shellMesh.GetBoundingBox(false));
-            Point3d shellCenter = shellBox.Center;
-
-            List<Point3d> checkPoints = new List<Point3d>(); // for testing loadPatterns FOR LIVELOAD
-
-            //foreach (Mesh shellMesh in shellMeshes)
-            for (int i = 0; i<shellMeshes.Count;i++)
-            {
-                Mesh shellMesh = shellMeshes[i];
-
-                //TEST for area adjustements
+                // if area factors, average them out for gravity - 
+                // should be done individually, but that makes the component complicated, needing volume and density
+                // to be revised for variable thickness
+                // OBSOLETE?
+                double areaFactorGravity = 0;
                 if (areaFactors.Count > 0)
                 {
-                    double areaFactor = areaFactors[i];
-                    deadLoadValue *= areaFactor;
-                    liveLoadValue *= areaFactor;
+                    foreach (double areaFactor in areaFactors)
+                        areaFactorGravity += areaFactor;
+                    areaFactorGravity /= areaFactors.Count; 
                 }
+                else
+                    areaFactorGravity = 1;
 
-                Mesh3 baseMesh = m.toBaseMesh(shellMesh.Convert());
+                // one for each load case
 
-                //----------- Gravity?
+                Load gravity = new GravityLoad (new Vector3(0, 0, -1 * areaFactorGravity * DL_FACTOR));
+                gravity.loadcase = loadCase;
+                k3dLoads.Add(gravity);
 
 
-                //----------- Dead load mesh load - same as Loads component > MeshLoad Const   
+                var k3dFL = new KarambaCommon.Factories.FactoryLoad();
+                UnitsConversionFactory ucf = UnitsConversionFactories.Conv();
+                UnitConversion m = ucf.m();
 
-                MeshLoad deadLoad = k3dFL.MeshLoad(new List<Vector3>() { new Vector3(0, 0, -(deadLoadValue * DL_FACTOR)) }, baseMesh, LoadOrientation.proj);
-                k3dLoads.Add(deadLoad);
+                // determine shell center for relative polar coordinates of face centers FOR LIVELOAD
+                BoundingBox shellBox = BoundingBox.Unset;
+                foreach (Mesh shellMesh in shellMeshes)
+                    shellBox = BoundingBox.Union (shellBox, shellMesh.GetBoundingBox(false));
+                Point3d shellCenter = shellBox.Center;
 
-                //----------- Live load mesh load, based on asymmetrical load pattern
+                //List<Point3d> checkPoints = new List<Point3d>(); // for testing loadPatterns FOR LIVELOAD
+
+                //foreach (Mesh shellMesh in shellMeshes)
+                for (int i = 0; i<shellMeshes.Count;i++)
+                {
+                    Mesh shellMesh = shellMeshes[i];
+
+                    //TEST for area adjustements
+                    if (areaFactors.Count > 0)
+                    {
+                        double areaFactor = areaFactors[i];
+                        deadLoadValue *= areaFactor;
+                        liveLoadValue *= areaFactor;
+                    }
+
+                    Mesh3 baseMesh = m.toBaseMesh(shellMesh.Convert());
+
+                    //----------- Gravity?
+
+
+                    //----------- Dead load mesh load - same as Loads component > MeshLoad Const   
+
+
+                    MeshLoad deadLoad = k3dFL.MeshLoad(new List<Vector3>() { new Vector3(0, 0, -(deadLoadValue * DL_FACTOR)) }, baseMesh, LoadOrientation.proj);
+                    deadLoad.loadcase = loadCase;
+                    k3dLoads.Add(deadLoad);
+
+
+
+                    //----------- Live load mesh load, based on asymmetrical load pattern
 
               
-                // get angles included in pattern
-                List<Vector3> liveLoadVectors = new List<Vector3>();
-                MeshFaceList k3dMeshFaces = shellMesh.Faces;
+                    // get angles included in pattern
+                    List<Vector3> liveLoadVectors = new List<Vector3>();
+                    MeshFaceList k3dMeshFaces = shellMesh.Faces;
 
-                for (int faceIndex = 0; faceIndex < shellMesh.Faces.Count; faceIndex ++)
-                {
-                    // get face center polar coordinate
-                    Point3d faceCenter = k3dMeshFaces.GetFaceCenter(faceIndex);
-                    Vector3d faceCenterOrientation = new Vector3d(faceCenter) - new Vector3d(shellCenter);
-                    double faceAngle = Math.Atan2(faceCenterOrientation.Y, faceCenterOrientation.X);
-                    if (faceAngle < 0) faceAngle += Math.PI * 2; // ensures angle always positive, [0, 2Pi] 
-
-                    // check if polar coordinate of face center is within load patterndomain,                
-                    if (AngleInPattern (faceAngle, loadPattern))
+                    for (int faceIndex = 0; faceIndex < shellMesh.Faces.Count; faceIndex ++)
                     {
-                        liveLoadVectors.Add(new Vector3(0, 0, -(liveLoadValue * LL_FACTOR)));
-                        checkPoints.Add(faceCenter); // for testing - remove in the end
+                        // get face center polar coordinate
+                        Point3d faceCenter = k3dMeshFaces.GetFaceCenter(faceIndex);
+                        Vector3d faceCenterOrientation = new Vector3d(faceCenter) - new Vector3d(shellCenter);
+                        double faceAngle = Math.Atan2(faceCenterOrientation.Y, faceCenterOrientation.X);
+                        if (faceAngle < 0) faceAngle += Math.PI * 2; // ensures angle always positive, [0, 2Pi] 
+
+                        // check if polar coordinate of face center is within load patterndomain,                
+                        if (AngleInPattern (faceAngle, loadPattern))
+                        {
+                            liveLoadVectors.Add(new Vector3(0, 0, -(liveLoadValue * LL_FACTOR)));
+                            //checkPoints.Add(faceCenter); // for testing - remove in the end
+                            checkPointTree.Append(new GH_Point (faceCenter), path);
+
+                        }
+
+                        else
+                            liveLoadVectors.Add(Vector3.Zero);
                     }
-                    
-                    else
-                        liveLoadVectors.Add(Vector3.Zero);
+
+                    MeshLoad liveLoad = k3dFL.MeshLoad(liveLoadVectors, baseMesh, LoadOrientation.proj);
+                    liveLoad.loadcase = loadCase;
+                    k3dLoads.Add(liveLoad);
+
+                    // increments loadCase
+
                 }
-
-                MeshLoad liveLoad = k3dFL.MeshLoad(liveLoadVectors, baseMesh, LoadOrientation.proj);
-                k3dLoads.Add(liveLoad);
-
+                loadCase++;
             }
-
             
 
             // convert from Karamba Loads to Karamba.GHopper Loads
@@ -165,7 +191,7 @@ namespace ACORN_shells
                 ghLoads.Add (new Karamba.GHopper.Loads.GH_Load(k3dLoad));
             
             DA.SetDataList(0, ghLoads);
-            DA.SetDataList(1, checkPoints);
+            DA.SetDataTree(1, checkPointTree);
         }
 
         protected override System.Drawing.Bitmap Icon
@@ -181,50 +207,17 @@ namespace ACORN_shells
             get { return new Guid("4e59b772-fbd4-4253-9d1b-c4c4b5b841af"); }
         }
 
-        public bool AngleInPattern(double angle, int loadPattern)
+        public bool AngleInPattern(double angle, Tuple<double, double> loadPatternTuple)
         {
-            // initialize for pattern 1
-            double arc = Math.PI * 2;
-            double offset = Math.PI * 2;
-
-            switch (loadPattern)
-            {
-                case 1:
-                    arc =    Math.PI * 2.00;
-                    offset = Math.PI * 2.00;
-                    break;
-                case 2:
-                    arc =    Math.PI * 1.00;
-                    offset = Math.PI * 0.50;
-                    break;
-                case 3:
-                    arc =    Math.PI * 1.00;
-                    offset = Math.PI * 0.25;
-                    break;
-                case 4:
-                    arc =    Math.PI * 0.50;
-                    offset = Math.PI * 0.50;
-                    break;
-                case 5:
-                    arc =    Math.PI * 0.50;
-                    offset = Math.PI * 0.25;
-                    break;
-                case 6:
-                    arc =    Math.PI * 0.25;
-                    offset = Math.PI * 0.00;
-                    break;
-                case 7:
-                    arc =    Math.PI * 2.00;
-                    offset = Math.PI * 0.00;
-                    break;
-            }
-
+            double arc = loadPatternTuple.Item1;
+            double offset = loadPatternTuple.Item2;
             double start = offset;
             double end;
             while (start < Math.PI * 2) // limits verification to one "lap"
             {
                 end = start + arc;
-                if (angle >= start && angle <= end) // angle within interval
+                // angle within interval
+                if ((angle >= start && angle <= end) || (angle <= offset - arc))
                     return true;
                 start += (2 * arc);
             }
